340c48a610734c49c9a06b6eea2a0dcd1e7ab349
Baranitharan26 <baranitharan45@gmail.com>
2017-06-16 16:17:54 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int start,end,i,flag=0,x;
+printf("enter the starting and ending interval");
+scanf("%d%d",&start,&end);
+for(i=start;i<=end;i++)
+{
+  for(x=2;x<i;x++)
+{
+if(i%x==0)
+{
+flag=1;
+}
+}
+if(flag==0)
+printf("%d",&i);
+}
+
+printf("the above is the prime number series");
+}
